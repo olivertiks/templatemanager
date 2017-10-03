d76e12481c35f8e95df76784678138d68ada7181
Karl Erik <k43gfx@gmail.com>
2017-10-03 03:44:45 +0300

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ENV['MONGO_SERVER'] = "127.0.0.1:27017"
+ENV['MONGO_SERVER'] = "127.0.0.1:27017" #local
